3ddb502b123e78ab79322a22d651465b4d4b25aa
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-08-19 17:05:40 +0100

    feat: store function

--- app/Services/BlogService.php
+++ app/Services/BlogService.php
@@ -4,5 +4,14 @@ namespace App\Services;
 use App\Models\Blog;
 
  class BlogService {
-    
+
+    public function store(array $data) {
+
+        $createdBlog = Blog::create($data);
+        Log::info('Blog created', [
+            'id' => $data->id,
+        ]);
+
+        return $createdBlog;
+    }
  }
