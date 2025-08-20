1eb7962adc6a8bab8c5380d760bc33cdaa78848d
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-08-20 22:17:13 +0100

    feat: added store method to BlogController

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Blog;
-
+use App\Services\BlogService;
 
 class BlogController extends Controller {
 
    public function index() {
     return Blog::query()->paginate(5);
    }
+   
+   public function store() {
 
-
-}
+   }
+   
+  }
