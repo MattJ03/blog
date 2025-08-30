b78d41c89cfe5e926fd9ce7cdeadf441e387a1d6
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-08-30 23:19:30 +0100

    feat: set up router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import { createApp } from 'vue';
+import App from 'resources/js/App.vue';
+import { createPinia } from 'pinia';
+
+const app = createApp(App);
+
+const pinia = createPinia();
+
+app.use(pinia);
+
+app.mount('#app');
