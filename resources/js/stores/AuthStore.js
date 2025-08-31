81492d0a9c3198888884bbba62f8fe6e5bba4ca7
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-08-31 19:09:30 +0100

    feat: login method in authstore

--- resources/js/stores/AuthStore.js
+++ resources/js/stores/AuthStore.js
@@ -11,6 +11,18 @@ const error = ref('');
 
 async function login(email, password) {
     loading.value = true;
+    try {
+        const res = await axios.post('api/login', {email, password});
+        user.value = res.data.user;
+        token.value = res.data.token;
+        localStorage.setItem('token', token.value);
+
+        axios.defaults.headers.common['Authorization'] = "Bearer ${token.value}";
+    } catch(error) {
+        console.log(error.response?.data || error.message);
+    } finally {
+        loading.value = false; 
+    }
 }
 
 });
