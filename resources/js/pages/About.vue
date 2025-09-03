6c15f4c5f58229c44ed100612e74455cd1c51b39
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-09-03 01:44:22 +0100

    feat: made about page, added to route

---
+++ resources/js/pages/About.vue
@@ -0,0 +1,7 @@
+<template>
+    </template>
+    <script setup>
+</script>
+<scoped style>
+    
+</scoped>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,9 +3,11 @@ import App from '../App.vue';
 import { createRouter } from 'vue-router';
 import { createWebHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
+import About from '../pages/About.vue';
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/about', component: About },
 ];
 
 const router = createRouter({
