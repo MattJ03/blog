9e9792c714842ed5c756bb3d390cb84e0135282e
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-09-14 19:18:57 +0100

    chore: added navbar to all pages

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -1,6 +1,8 @@
 <template>
+    <NavBar></NavBar>
     </template>
     <script setup>
+    import NavBar from '../components/NavBar.vue';
 </script>
 <scoped style>
     
--- resources/js/pages/Projects.vue
+++ resources/js/pages/Projects.vue
@@ -1,6 +1,8 @@
 <template>
+    <NavBar></NavBar>
     </template>
     <script setup>
+    import NavBar from '../components/NavBar.vue';
 </script>
 <style scoped>
 </style>
